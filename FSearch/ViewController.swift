1babcf9cb5a0e8fbecf9c43bc0a04696cec361e6
Kru178 <s.krupe@gmail.com>
2023-08-03 22:12:46 +0200

    Initial Commit

---
+++ FSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FSearch
+//
+//  Created by Sergei Krupenikov on 03.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
